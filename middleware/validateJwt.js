f0dd4b2938b78fbe948b047cd2b7293ec0eb25a9
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-06-27 20:41:13 -0500

    validate jwt added

---
+++ middleware/validateJwt.js
@@ -0,0 +1,33 @@
+const { response } = require("express");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const validateJwt = async (req, res = response, next) => {
+  const token = req.header("token");
+
+  if (!token) {
+    return res.status(401).json("Enter a token in the request");
+  }
+
+  try {
+    const { uid } = jwt.verify(token, process.env.JWTPRIVATEKEY);
+
+    const user = await User.findById(uid);
+
+    if (!user) {
+      return res.status(401).json({ msg: "User does not exist invalid token" });
+    }
+
+    if (!user.status) {
+      return res.status(401).json({ msg: "Invalid token - status false" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
+module.exports = { validateJwt };
